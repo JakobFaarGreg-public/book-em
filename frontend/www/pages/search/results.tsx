74ea8ca6097cd45cf5a891cee92eda3ed227850c
Jakob Faarbæk Gregersen <lilleper.jfg@gmail.com>
2024-04-19 17:27:14 +0000

    feat: integrate with get book api closes #16

--- frontend/www/pages/search/results.tsx
+++ frontend/www/pages/search/results.tsx
@@ -1,20 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import axios from "axios";
+import SearchBar from "../../components/SearchBar";
 const SearchResultsPage = () => {
-  const router = useRouter()
-  const { book} = router.query
+  const [data, setData] = useState();
+  const router = useRouter();
+  const { book } = router.query;
   useEffect(() => {
     console.log(book);
+    getBooks(book);
+  }, []);
 
-  },[])
+  const getBooks = async (book) => {
+    axios
+      .get(`http://127.0.0.1:3333/v1/books?book=${book}`)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => console.error(error));
+  };
+
   return (
     <>
       <div id="Sidebar">
         <Link href={"/"}>Back to home</Link>
-        <div>Search Results</div>
+        <SearchBar />
       </div>
-      <div id="ScrollableBookList"></div>
     </>
   );
 };
